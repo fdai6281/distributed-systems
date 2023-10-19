9776dd728342f3f03df8548821e624543cd74501
Nick Stolbov <nick.stolbov@informatik.hs-fulda.de>
2023-10-19 17:58:28 +0200

    -init

---
+++ src/main/java/com/distributedsystems/bmicalc/BmiCalcApplication.java
@@ -0,0 +1,13 @@
+package com.distributedsystems.bmicalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distributedsystems/bmicalc/BmiCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedsystems.bmicalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
